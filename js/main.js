af11aec36b9b126587a10d0c7c1bcd3b198371b2
unknown <mesuraj30@gmail.com>
2020-04-23 15:00:46 +0530

    nav-menu-1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
+    $toggleIcon = $('.toggle-icons i');
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
+        $toggleIcon.toggleClass('border');
     })
 });
